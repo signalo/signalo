ef35d7ce82bdfcd8bc7804905196ebb0bd6d1cac
Vincent Esche <regexident@gmail.com>
2018-04-23 12:47:20 +0200

    Added `missing_mpl` lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(feature = "nightly", feature(try_from))]
 
+#![cfg_attr(feature = "missing_mpl", feature(plugin))]
+#![cfg_attr(feature = "missing_mpl", plugin(missing_mpl))]
+#![cfg_attr(feature = "missing_mpl", deny(missing_mpl))]
+
 extern crate num_traits;
 extern crate num_integer;
 
