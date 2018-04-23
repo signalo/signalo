f1c9ee0c8aff445f32249f9f5bd57de3be1933c6
Vincent Esche <regexident@gmail.com>
2018-04-23 14:15:16 +0200

    Added `Square` filter

--- src/filter/ops/mod.rs
+++ src/filter/ops/mod.rs
@@ -3,7 +3,11 @@ mod sub;
 mod mul;
 mod div;
 
+mod square;
+
 pub use self::add::Add;
 pub use self::sub::Sub;
 pub use self::mul::Mul;
 pub use self::div::Div;
+
+pub use self::square::Square;
---
+++ src/filter/ops/square.rs
@@ -0,0 +1,63 @@
+use std::ops::BitOr;
+use std::ops::Mul;
+
+use filter::pipe::Pipe;
+use filter::Filter;
+
+#[derive(Clone, Debug)]
+pub struct Square;
+
+impl Square {
+    #[inline]
+    pub fn new() -> Self {
+        Square
+    }
+}
+
+impl<Rhs> BitOr<Rhs> for Square {
+    type Output = Pipe<Self, Rhs>;
+
+    #[inline]
+    fn bitor(self, filter: Rhs) -> Self::Output {
+        Pipe::new(self, filter)
+    }
+}
+
+impl<T> Filter<T> for Square
+where
+    T: Copy + Mul<T>
+{
+    type Output = <T as Mul<T>>::Output;
+
+    #[inline]
+    fn apply(&mut self, input: T) -> Self::Output {
+        input * input
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fixed_point() {
+        let filter = Square::new();
+        // Sequence: https://en.wikipedia.org/wiki/Collatz_conjecture
+        let input = vec![0, 1, 7, 2, 5, 8, 16, 3, 19, 6, 14, 9, 9, 17, 17, 4, 12, 20, 20, 7];
+        let output: Vec<_> = input.iter().scan(filter, |filter, &input| {
+            Some(filter.apply(input))
+        }).collect();
+        assert_eq!(output, vec![0, 1, 49, 4, 25, 64, 256, 9, 361, 36, 196, 81, 81, 289, 289, 16, 144, 400, 400, 49]);
+    }
+
+    #[test]
+    fn floating_point() {
+        let filter = Square::new();
+        // Sequence: https://en.wikipedia.org/wiki/Collatz_conjecture
+        let input = vec![0.0, 1.0, 7.0, 2.0, 5.0, 8.0, 16.0, 3.0, 19.0, 6.0, 14.0, 9.0, 9.0, 17.0, 17.0, 4.0, 12.0, 20.0, 20.0, 7.0];
+        let output: Vec<_> = input.iter().scan(filter, |filter, &input| {
+            Some(filter.apply(input))
+        }).collect();
+        assert_nearly_eq!(output, vec![0.0, 1.0, 49.0, 4.0, 25.0, 64.0, 256.0, 9.0, 361.0, 36.0, 196.0, 81.0, 81.0, 289.0, 289.0, 16.0, 144.0, 400.0, 400.0, 49.0]);
+    }
+}
