eb8999c8396e4d49501c23be473fb0067bef7880
Vincent Esche <regexident@gmail.com>
2018-05-22 11:47:09 +0200

    Added proper `Median` filter (vs. approximated exponential median)

--- filters/src/filter/median/mod.rs
+++ filters/src/filter/median/mod.rs
@@ -5,3 +5,38 @@
 //! Median filters.
 
 pub mod exp;
+
+mod median;
+
+pub use self::median::Median1;
+pub use self::median::Median2;
+pub use self::median::Median3;
+pub use self::median::Median4;
+pub use self::median::Median5;
+pub use self::median::Median6;
+pub use self::median::Median7;
+pub use self::median::Median8;
+pub use self::median::Median9;
+pub use self::median::Median10;
+pub use self::median::Median11;
+pub use self::median::Median12;
+pub use self::median::Median13;
+pub use self::median::Median14;
+pub use self::median::Median15;
+pub use self::median::Median16;
+pub use self::median::Median17;
+pub use self::median::Median18;
+pub use self::median::Median19;
+pub use self::median::Median20;
+pub use self::median::Median21;
+pub use self::median::Median22;
+pub use self::median::Median23;
+pub use self::median::Median24;
+pub use self::median::Median25;
+pub use self::median::Median26;
+pub use self::median::Median27;
+pub use self::median::Median28;
+pub use self::median::Median29;
+pub use self::median::Median30;
+pub use self::median::Median31;
+pub use self::median::Median32;
