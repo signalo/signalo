49e011472ea76d8c68a9933fe39e942039e2e6f4
Vincent Esche <regexident@gmail.com>
2018-06-26 15:57:29 +0200

    Added one more missing crate documentation

--- signalo/src/lib.rs
+++ signalo/src/lib.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+//! The 'signalo' umbrella crate.
+
 #![cfg_attr(feature = "missing_mpl", feature(plugin))]
 #![cfg_attr(feature = "missing_mpl", plugin(missing_mpl))]
 #![cfg_attr(feature = "missing_mpl", deny(missing_mpl))]
