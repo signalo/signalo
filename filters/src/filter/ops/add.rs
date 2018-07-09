5f03461d3e733f0d27e524e547467e1f6a96d7da
Vincent Esche <regexident@gmail.com>
2018-07-09 12:01:52 +0200

    Refactored `ops` filters from stateful unary to state-less binary filters

--- filters/src/filter/ops/add.rs
+++ filters/src/filter/ops/add.rs
@@ -6,36 +6,20 @@ use std::ops::Add as StdAdd;
 
 use signalo_traits::filter::Filter;
 
-/// A filter performing addition of a signal and a constant value.
-#[derive(Clone, Debug)]
-pub struct Add<T> {
-    value: T
-}
+/// A filter performing `+` on a pair of values.
+#[derive(Default, Clone, Debug)]
+pub struct Add;
 
-impl<T> Add<T> {
-    /// Creates a `Add` filter for a given `value`.
-    #[inline]
-    pub fn new(value: T) -> Self {
-        Add { value }
-    }
-
-    /// Returns the filter's value.
-    #[inline]
-    pub fn value(&self) -> &T {
-        &self.value
-    }
-}
-
-impl<T, U> Filter<U> for Add<T>
+impl<T, U> Filter<(T, U)> for Add
 where
-    T: Copy,
-    U: StdAdd<T>,
+    T: Copy + StdAdd<U>,
 {
-    type Output = <U as StdAdd<T>>::Output;
+    type Output = <T as StdAdd<U>>::Output;
 
     #[inline]
-    fn filter(&mut self, input: U) -> Self::Output {
-        input + self.value
+    fn filter(&mut self, input: (T, U)) -> Self::Output {
+        let (lhs, rhs) = input;
+        lhs + rhs
     }
 }
 
@@ -43,25 +27,32 @@ where
 mod tests {
     use super::*;
 
-    #[test]
-    fn fixed_point() {
-        let filter = Add::new(42);
-        // Sequence: https://en.wikipedia.org/wiki/Collatz_conjecture
-        let input = vec![0, 1, 7, 2, 5, 8, 16, 3, 19, 6, 14, 9, 9, 17, 17, 4, 12, 20, 20, 7];
-        let output: Vec<_> = input.iter().scan(filter, |filter, &input| {
-            Some(filter.filter(input))
-        }).collect();
-        assert_eq!(output, vec![42, 43, 49, 44, 47, 50, 58, 45, 61, 48, 56, 51, 51, 59, 59, 46, 54, 62, 62, 49]);
+    fn get_input() -> Vec<f32> {
+        vec![
+            0.0, 1.0, 7.0, 2.0, 5.0, 8.0, 16.0, 3.0, 19.0, 6.0, 14.0, 9.0, 9.0, 17.0, 17.0, 4.0,
+            12.0, 20.0, 20.0, 7.0, 7.0, 15.0, 15.0, 10.0, 23.0, 10.0, 111.0, 18.0, 18.0, 18.0,
+            106.0, 5.0, 26.0, 13.0, 13.0, 21.0, 21.0, 21.0, 34.0, 8.0, 109.0, 8.0, 29.0, 16.0,
+            16.0, 16.0, 104.0, 11.0, 24.0, 24.0
+        ]
+    }
+
+    fn get_output() -> Vec<f32> {
+        vec![
+            4.200, 5.200, 11.200, 6.200, 9.200, 12.200, 20.200, 7.200, 23.200, 10.200, 18.200,
+            13.200, 13.200, 21.200, 21.200, 8.200, 16.200, 24.200, 24.200, 11.200, 11.200,
+            19.200, 19.200, 14.200, 27.200, 14.200, 115.200, 22.200, 22.200, 22.200, 110.200,
+            9.200, 30.200, 17.200, 17.200, 25.200, 25.200, 25.200, 38.200, 12.200, 113.200,
+            12.200, 33.200, 20.200, 20.200, 20.200, 108.200, 15.200, 28.200, 28.200
+        ]
     }
 
     #[test]
-    fn floating_point() {
-        let filter = Add::new(4.2);
-        // Sequence: https://en.wikipedia.org/wiki/Collatz_conjecture
-        let input = vec![0.0, 1.0, 7.0, 2.0, 5.0, 8.0, 16.0, 3.0, 19.0, 6.0, 14.0, 9.0, 9.0, 17.0, 17.0, 4.0, 12.0, 20.0, 20.0, 7.0];
+    fn div() {
+        let filter = Add::default();
+        let input = get_input();
         let output: Vec<_> = input.iter().scan(filter, |filter, &input| {
-            Some(filter.filter(input))
+            Some(filter.filter((input, 4.2)))
         }).collect();
-        assert_nearly_eq!(output, vec![4.2, 5.2, 11.2, 6.2, 9.2, 12.2, 20.2, 7.2, 23.2, 10.2, 18.2, 13.2, 13.2, 21.2, 21.2, 8.2, 16.2, 24.2, 24.2, 11.2]);
+        assert_nearly_eq!(output, get_output(), 0.001);
     }
 }
--- filters/src/filter/ops/sub.rs
+++ filters/src/filter/ops/sub.rs
@@ -6,36 +6,20 @@ use std::ops::Sub as StdSub;
 
 use signalo_traits::filter::Filter;
 
-/// A filter performing subtraction of a signal and a constant value.
-#[derive(Clone, Debug)]
-pub struct Sub<T> {
-    value: T
-}
+/// A filter performing `-` on a pair of values.
+#[derive(Default, Clone, Debug)]
+pub struct Sub;
 
-impl<T> Sub<T> {
-    /// Creates a `Sub` filter for a given `value`.
-    #[inline]
-    pub fn new(value: T) -> Self {
-        Sub { value }
-    }
-
-    /// Returns the filter's value.
-    #[inline]
-    pub fn value(&self) -> &T {
-        &self.value
-    }
-}
-
-impl<T, U> Filter<U> for Sub<T>
+impl<T, U> Filter<(T, U)> for Sub
 where
-    T: Copy,
-    U: StdSub<T>,
+    T: Copy + StdSub<U>,
 {
-    type Output = <U as StdSub<T>>::Output;
+    type Output = <T as StdSub<U>>::Output;
 
     #[inline]
-    fn filter(&mut self, input: U) -> Self::Output {
-        input - self.value
+    fn filter(&mut self, input: (T, U)) -> Self::Output {
+        let (lhs, rhs) = input;
+        lhs - rhs
     }
 }
 
@@ -43,23 +27,32 @@ where
 mod tests {
     use super::*;
 
-    #[test]
-    fn fixed_point() {
-        let filter = Sub::new(42);
-        let input = vec![42, 43, 49, 44, 47, 50, 58, 45, 61, 48, 56, 51, 51, 59, 59, 46, 54, 62, 62, 49];
-        let output: Vec<_> = input.iter().scan(filter, |filter, &input| {
-            Some(filter.filter(input))
-        }).collect();
-        assert_eq!(output, vec![0, 1, 7, 2, 5, 8, 16, 3, 19, 6, 14, 9, 9, 17, 17, 4, 12, 20, 20, 7]);
+    fn get_input() -> Vec<f32> {
+        vec![
+            0.0, 1.0, 7.0, 2.0, 5.0, 8.0, 16.0, 3.0, 19.0, 6.0, 14.0, 9.0, 9.0, 17.0, 17.0, 4.0,
+            12.0, 20.0, 20.0, 7.0, 7.0, 15.0, 15.0, 10.0, 23.0, 10.0, 111.0, 18.0, 18.0, 18.0,
+            106.0, 5.0, 26.0, 13.0, 13.0, 21.0, 21.0, 21.0, 34.0, 8.0, 109.0, 8.0, 29.0, 16.0,
+            16.0, 16.0, 104.0, 11.0, 24.0, 24.0
+        ]
+    }
+
+    fn get_output() -> Vec<f32> {
+        vec![
+            -4.200, -3.200, 2.800, -2.200, 0.800, 3.800, 11.800, -1.200, 14.800, 1.800, 9.800,
+            4.800, 4.800, 12.800, 12.800, -0.200, 7.800, 15.800, 15.800, 2.800, 2.800, 10.800,
+            10.800, 5.800, 18.800, 5.800, 106.800, 13.800, 13.800, 13.800, 101.800, 0.800,
+            21.800, 8.800, 8.800, 16.800, 16.800, 16.800, 29.800, 3.800, 104.800, 3.800, 24.800,
+            11.800, 11.800, 11.800, 99.800, 6.800, 19.800, 19.800
+        ]
     }
 
     #[test]
-    fn floating_point() {
-        let filter = Sub::new(4.2);
-        let input = vec![4.2, 5.2, 11.2, 6.2, 9.2, 12.2, 20.2, 7.2, 23.2, 10.2, 18.2, 13.2, 13.2, 21.2, 21.2, 8.2, 16.2, 24.2, 24.2, 11.2];
+    fn div() {
+        let filter = Sub::default();
+        let input = get_input();
         let output: Vec<_> = input.iter().scan(filter, |filter, &input| {
-            Some(filter.filter(input))
+            Some(filter.filter((input, 4.2)))
         }).collect();
-        assert_nearly_eq!(output, vec![0.0, 1.0, 7.0, 2.0, 5.0, 8.0, 16.0, 3.0, 19.0, 6.0, 14.0, 9.0, 9.0, 17.0, 17.0, 4.0, 12.0, 20.0, 20.0, 7.0]);
+        assert_nearly_eq!(output, get_output(), 0.001);
     }
 }
