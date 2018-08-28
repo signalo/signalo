ded62a1d2e48fbb8a4e11f31f3c9bc32ab1c2a2a
Vincent Esche <regexident@gmail.com>
2018-08-28 16:23:10 +0200

    Improved state-handling in sinks

--- filters/src/sink/mean_variance.rs
+++ filters/src/sink/mean_variance.rs
@@ -2,63 +2,74 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use num_traits::{Num, One, Zero};
+use num_traits::Num;
 
 use signalo_traits::sink::Sink;
 
-/// A sink that computes the mean and variance of all received values of a signal.
 #[derive(Clone, Debug)]
-pub struct MeanVariance<T> {
-    count: T,    // number of values seen so far
-    mean: T,     // mean of values seen so far
-    variance: T, // squared distance from the mean
+struct State<T> {
+    // number of values seen so far
+    count: T,
+    // mean of values seen so far
+    mean: T,
+    // squared distance from the mean
+    variance: T,
 }
 
-impl<T> MeanVariance<T>
-where
-    T: Zero,
-{
-    /// Creates a new `MeanVariance` sink.
-    #[inline]
-    pub fn new() -> Self {
-        let count = T::zero();
-        let mean = T::zero();
-        let variance = T::zero();
-        MeanVariance {
-            count,
-            mean,
-            variance,
-        }
-    }
+/// A sink that computes the mean and variance of all received values of a signal.
+#[derive(Clone, Default, Debug)]
+pub struct MeanVariance<T> {
+    // The sink's state
+    state: Option<State<T>>,
 }
 
 impl<T> Sink<T> for MeanVariance<T>
 where
-    T: Copy + PartialOrd + One + Num,
+    T: Copy + PartialOrd + Num,
 {
-    type Output = (T, T);
+    type Output = Option<(T, T)>;
 
     #[inline]
     fn sink(&mut self, input: T) {
-        let count = self.count + T::one();
-        let delta = input - self.mean;
-        let mean = self.mean + (delta / count);
-        let delta2 = input - mean;
-        let variance = self.variance + (delta * delta2);
+        let (old_count, old_mean, old_variance) = match self.state {
+            Some(State {
+                count,
+                mean,
+                variance,
+            }) => (count, mean, variance),
+            None => (T::zero(), T::zero(), T::zero()),
+        };
 
-        self.count = count;
-        self.mean = mean;
-        self.variance = variance;
+        let count = old_count + T::one();
+        let mean = old_mean + ((input - old_mean) / count);
+
+        let old_delta = input - old_mean;
+        let delta = input - mean;
+
+        let variance = old_variance + (old_delta * delta);
+
+        self.state = Some(State {
+            count,
+            mean,
+            variance,
+        });
     }
 
     #[inline]
     fn finalize(self) -> Self::Output {
-        let variance = if self.count > T::one() {
-            self.variance / (self.count - T::one())
-        } else {
-            self.variance
-        };
-        (self.mean, variance)
+        self.state.map(|state| {
+            let State {
+                count,
+                mean,
+                variance,
+            } = state;
+            let variance = if count > T::one() {
+                variance / (count - T::one())
+            } else {
+                variance
+            };
+            (mean, variance)
+        })
     }
 }
 
@@ -75,13 +86,15 @@ mod tests {
             106.0, 5.0, 26.0, 13.0, 13.0, 21.0, 21.0, 21.0, 34.0, 8.0, 109.0, 8.0, 29.0, 16.0,
             16.0, 16.0, 104.0, 11.0, 24.0, 24.0,
         ];
-        let mut sink = MeanVariance::new();
+        let mut sink = MeanVariance::default();
         for input in input {
             sink.sink(input);
         }
-        println!("\n{:#?}\n", sink);
-        let (mean, variance) = sink.finalize();
-        assert_nearly_eq!(mean, 26.56);
-        assert_nearly_eq!(variance, 1347.68);
+        if let Some((mean, variance)) = sink.finalize() {
+            assert_nearly_eq!(mean, 26.56);
+            assert_nearly_eq!(variance, 1347.68);
+        } else {
+            panic!("Expected Some(…), found None.");
+        }
     }
 }
