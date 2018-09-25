b4477d4eca9f017750077fa8df6bb37a5ca914bd
Vincent Esche <regexident@gmail.com>
2018-09-25 21:17:23 +0200

    Added missing module documentation

--- filters/src/filter/identity.rs
+++ filters/src/filter/identity.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+//! Identify function filters.
+
 use signalo_traits::filter::Filter;
 
 /// A filter that simply returns the values it receives.
